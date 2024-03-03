4d224c5a1d102a68bc7a8f37614fb2d18a6f3294
Taylor Laird <taylorolaird@knights.ucf.edu>
2024-03-02 20:45:23 -0500

    finished jumping

--- Assets/FlyMove.cs
+++ Assets/FlyMove.cs
@@ -9,6 +9,9 @@ public class FlyMove : MonoBehaviour
     // reference to the the object to get what direction to fly
     public Transform objectReference;
 
+    // public variable for fly speed
+    public float flySpeed = 1.0f;
+
     private void OnEnable()
     {
         FlyButton.action.Enable();
@@ -24,10 +27,14 @@ public class FlyMove : MonoBehaviour
         // Check if the button is pressed
         if (FlyButton.action.ReadValue<float>() > 0.5f)
         {
-            // move the object in the 
-            transform.position += (objectReference.forward * .1f);
+            // why are we multiplying by Time.deltaTime?
+            // Time.deltaTime is the time it took to complete the last frame
+            transform.position += objectReference.forward * flySpeed;
+        }
+        else
+        {
+            // set the players velocity to 0 to stop the player from moving
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
     }
 }
-
-
